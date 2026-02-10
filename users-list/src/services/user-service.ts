e561dc17bc338ff0eca868062272a160bfcd2314
Mohammad Polaei <mohammadpolaei@gmail.com>
2026-02-11 01:54:02 +0330

    feat:services and custom hooks bases created

---
+++ users-list/src/services/user-service.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import type { User } from "../types/types";
+
+export const api = axios.create({
+	baseURL: "https://698bac606c6f9ebe57bd50dc.mockapi.io/users",
+	timeout: 5000,
+});
+
+export const getUsers = async () => {
+	const result = await axios.get("/");
+	return result.data;
+};
+
+export const createUser = async (user: User) => {
+	const result = await api.post("/", user);
+	return result.data;
+};
